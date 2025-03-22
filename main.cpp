3c3a761f88a1548ed07c4b06b9ed7385c40febdf
Ahlyab <ahlyabasad@gmail.com>
2025-03-22 15:21:06 +0500

    sum func

--- main.cpp
+++ main.cpp
@@ -6,8 +6,18 @@ void sayHello()
     cout << "Hello,WOrld!" << endl;
 }
 
+int sum(int a, int b, int c)
+{
+    return a + b + c;
+}
+
 int main()
 {
     sayHello();
+    int a, b, c;
+    cout << "Enter 3 numbers separated by space  : ";
+    cin >> a >> b >> c;
+
+    cout << "Result : " << sum(a, b, c);
     return 0;
 }
